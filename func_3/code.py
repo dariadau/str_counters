67179cdaa11b015f07aa6602de2ec5fea78449e4
dariadau <qwertykoko12@gmail.com>
2025-01-11 17:51:13 +1100

    new package has been created

--- func_3/code.py
+++ func_3/code.py
@@ -1,11 +1,14 @@
 def check_index(input_line="one two one tho three"):
     words_dict = {}
-    count_lst = [-1] * len(input_line.split())
+    count_lst = ['-1'] * len(input_line.split())
 
     for idx, word in enumerate(input_line.split()):
         if word not in words_dict:
             words_dict[word] = idx
         else:
-            count_lst[idx] = words_dict[word]
+            count_lst[idx] = str(words_dict[word])
             words_dict[word] = idx
-    return count_lst
+    return ', '.join(count_lst)
+
+
+print(check_index())
